b48c8499b4b4ff767dd88fa3bb1622f2c791fa98
mattgstevens <mattgstevens@gmail.com>
2018-07-15 22:21:18 +0200

    feat: gpg

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,29 @@ const autoprefix = new (require('less-plugin-autoprefix'))({
 })
 const server = require('gulp-server-livereload')
 
-gulp.task('less', () => (
-  gulp.src('./styles/index.less')
-    .pipe(less({
-      plugins: [autoprefix]
-    }))
+gulp.task('less', () =>
+  gulp
+    .src('./styles/index.less')
+    .pipe(
+      less({
+        plugins: [autoprefix]
+      })
+    )
     .pipe(gulp.dest('.'))
-))
-
-gulp.task('watch', () => (
-  gulp.watch('./styles/*.less', ['less']))
 )
+
+gulp.task('watch', () => gulp.watch('./styles/*.less', ['less']))
 
 gulp.task('build', ['less'])
 
-gulp.task('server', ['watch', 'build'], () => (
-  gulp.src('.')
-    .pipe(server({
+gulp.task('server', ['watch', 'build'], () =>
+  gulp.src('.').pipe(
+    server({
       livereload: {
         enable: true,
         filter: (filename, cb) => cb(!/\.less$/.test(filename))
       },
-      open: true,
-    }))
-))
+      open: true
+    })
+  )
+)
